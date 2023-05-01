755950b2fc8cf1cec199c8c2e721c08fc60ceb0d
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-05-01 21:54:59 +0200

    fix: electron subroutine

--- app/renderer/app.tsx
+++ app/renderer/app.tsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export class App extends Component {
 	render() {
 		return (
-			<div>
+			<div className="d-flex container-fluid min-vh-100 flex-column align-items-center justify-content-center">
 				<h1>Hi from React 🎉</h1>
 				<h2>Bootstrap and Webpack</h2>
-				<button className="btn btn-danger">Primary button</button>
+				<button className="btn btn-success">Primary button</button>
 			</div>
 		);
 	}
--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -4,33 +4,46 @@ import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { main, renderer } from './webpack.common';
 import { PATH } from './webpack.path';
 import { rendererRules } from './webpack.rules';
+import { spawn } from 'child_process';
 
 export default [
 	merge(main, {
 		mode: 'development',
 		devtool: 'inline-source-map'
-  }),
+	}),
 	merge(renderer, {
 		mode: 'development',
 		devtool: 'inline-source-map',
 		module: {
 			rules: rendererRules
-    },
+		},
 		optimization: {
 			runtimeChunk: 'single'
-    },
+		},
 		externalsPresets: { node: true },
 		// @ts-ignore
 		devServer: {
 			static: PATH.OUTPUT,
 			port: 4000,
-			hot: true
-    },
+			hot: true,
+			setupMiddlewares(middlewares: any) {
+				// Start electron subprocess
+				spawn('yarn start:main ', {
+					stdio: 'inherit',
+					shell: true
+				})
+					.on('close', (code: number) => {
+						process.exit(code!);
+					})
+					.on('error', (spawnError) => console.error(spawnError));
+				return middlewares;
+			}
+		},
 		plugins: [
 			new ForkTsCheckerWebpackPlugin(),
 			new DefinePlugin({
 				global: 'globalThis'
-      }),
-    ]
+			})
+		]
 	})
 ];
