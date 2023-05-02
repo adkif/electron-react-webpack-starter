f4a1f3470666a22c790fea0339c512032011ee71
Adolphe Kifungo <45813955+adkif@users.noreply.github.com>
2023-05-02 08:43:17 +0200

    fix: prettier format

--- app/renderer/app.tsx
+++ app/renderer/app.tsx
@@ -6,7 +6,7 @@ export class App extends Component {
 			<div className="d-flex container-fluid min-vh-100 flex-column align-items-center justify-content-center">
 				<h1>Hi from React 🎉</h1>
 				<h2>Bootstrap and Webpack</h2>
-				<button className="btn btn-success">Primary button</button>
+				<button className="btn btn-primary">Primary button</button>
 			</div>
 		);
 	}
